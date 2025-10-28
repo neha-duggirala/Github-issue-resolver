6bb03a68af4d2cbf6615cd85b2f550e2c628123f
neha-duggirala <nsm.duggirala@gmail.com>
2025-10-28 11:23:56 +0530

    check if its taking the env secrets

--- github_pr_reviewer/bin.py
+++ github_pr_reviewer/bin.py
@@ -1,4 +1,16 @@
 import os
-if "MODEL" in os.environ:
-    model = os.environ["MODEL"]
-    print(model)
+from google.genai import types
+from google import genai
+
+model = os.environ["MODEL"]
+api_key = os.environ["GOOGLE_API_KEY"]
+
+
+client = genai.Client(api_key= api_key)
+response = client.models.generate_content(
+    model=model,
+    contents=[
+       types.Content(role="user", parts =[types.Part(text="Hello, who are you!")])
+    ],
+)
+print("Response:", response.text)
